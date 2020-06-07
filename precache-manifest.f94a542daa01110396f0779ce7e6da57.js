db96f966a0ff0ba04a5a0cb8581aceab56ae86ef
PraneethNamala <praneeth.namala@credencys.com>
2020-06-07 11:22:43 +0530

    Updates

---
+++ precache-manifest.f94a542daa01110396f0779ce7e6da57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3a70314bbd33fc1f93b965b78e5a54",
+    "url": "/QuizzApp/index.html"
+  },
+  {
+    "revision": "c5d5dc101da333e8fe90",
+    "url": "/QuizzApp/static/css/main.bea45cab.chunk.css"
+  },
+  {
+    "revision": "2a4c5d30268f55fa3c51",
+    "url": "/QuizzApp/static/js/2.5c3d9928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizzApp/static/js/2.5c3d9928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d5dc101da333e8fe90",
+    "url": "/QuizzApp/static/js/main.f4aa9fdd.chunk.js"
+  },
+  {
+    "revision": "3474eab42cf15c41a9ba",
+    "url": "/QuizzApp/static/js/runtime-main.f35c11cd.js"
+  }
+]);
